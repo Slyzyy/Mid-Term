70d9f58fc372fc654704706fd4f951d91fb697dc
Muhammad Auzan Zaky Eryana <auzanzaky63@gmail.com>
2023-07-28 00:41:52 +0700

    Menambahkan API get video

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const Songs = require('../models/songs');
+const Video = require('../models/songs');
 
 router.post('/post', (req, res) => {
-    const songs = new Songs({
-        title : req.body.title,
-        artist : req.body.artist
+    const video = new Video({
+        username : req.body.username,
+        comment : req.body.comment,
+        videoID : req.body.videoID
     })
 
     try {
-        const songsToSave = songs.save();
-        res.status(200).json(songsToSave)
+        const videotoSave = video.save();
+        res.status(200).json(videotoSave)
     }
     catch (error) {
         res.status(400).json({message: error.message})
     }
 })
+
+router.get('/getVideo', async (req, res) => {
+    try{
+        const video = await Video.find();
+        res.json(video)
+    }
+    catch(error) {
+        res.status(500).json({message: error.message})
+    }
+});
 
 module.exports = router;
