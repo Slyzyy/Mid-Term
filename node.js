272fde5b4618baaa4bdab1106b3299caa1d5b737
Muhammad Auzan Zaky Eryana <auzanzaky63@gmail.com>
2023-07-27 23:20:10 +0700

    Menginisialisasi Express, MongoDB, dan set up Database

---
+++ node.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express')
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected')
+})
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
